29505d4fc19bedf74f88ac0a7f395287aafd2c5e
mourot_l <leomourot@gmail.com>
2013-01-14 16:41:03 +0100

    Add full check

--- tools_check.c
+++ tools_check.c
@@ -11,3 +11,30 @@ void	check_left(t_terrain* button, t_terrain* terrain)
 		return (1);
 	return (0);
 }
+
+void	check_right(t_terrain* button, t_terrain* terrain)
+{
+	while (terrain->x != button->x + 1 && terrain->y != button->y)
+		terrain = terrain->next;
+	if (terrain->num == 0)
+		return (1);
+	return (0);
+}
+
+void	check_top(t_terrain* button, t_terrain* terrain)
+{
+	while (terrain->x != button->x && terrain->y != button->y - 1)
+		terrain = terrain->next;
+	if (terrain->num == 0)
+		return (1);
+	return (0);
+}
+
+void	check_bot(t_terrain* button, t_terrain* terrain)
+{
+	while (terrain->x != button->x && terrain->y != button->y + 1)
+		terrain = terrain->next;
+	if (terrain->num == 0)
+		return (1);
+	return (0);
+}
